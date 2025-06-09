9e49d35940aa27174d46cedb8c1a60dba2ab2238
Aleksandr834 <sasha.zaitse@yandex.ru>
2025-06-09 21:04:09 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WheelHub</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+</head>
+<body>
+    <header class="header">
+        <div class="conteniar">
+            <div class="header-nav">
+                <img src="img/logo.png" alt="" width="200px" height="130px">
+                <nav class="nav">
+                    <ul class="nav-list">
+                        <li><a href="#cat">Каталог</a></li>
+                        <li><a href="#komn">О компании</a></li>
+                        <li><a href="#contact">Контакты</a></li>
+                    </ul>
+
+                    <div class="icon">
+                        <img src="img/icon/icon-user.png" alt="" width="30px" height="30px">
+                        <img src="img/icon/icon-corzina.png" alt="" width="30px" height="30px">
+                    </div>
+                </nav>
+            </div>
+            <div class="header-center">
+                <div class="header-content">
+                    <h1>Добро пожаловать в WheelHub</h1>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <main class="main">
+        <div class="conteniar" id="cat" >
+            <div class="main-text1">
+                <h1>Вы можете купить</h1>
+            </div>
+            <div class="cartohki">
+                <div class="cartochka">
+                    <img src="img/photo2_cov_glavn.jpg" alt="">
+                    <div class="text-over">Кованные диски</div>
+                </div>
+                <div class="cartochka">
+                    <img src="img/photo3_lit_glavn.jpg" alt="">
+                    <div class="text-over">Литые диски</div>
+                </div>
+                <div class="cartochka">
+                    <img class="image-raz" src="img/photo4_raz_glavn.jpg" alt="">
+                    <div class="text-over">Разборные диски</div>
+                </div>
+            </div>
+
+            <div class="hut">
+                <h1>Хиты продаж</h1>
+                <div class="cartochki2">
+                    <div class="cartochka2">
+                        <img class="photo1" src="img/cov_dicki/cov_2.png" alt="" width="250px">
+                        <div class="text-cartochka2">
+                            <h2>Колесный диск КиК Хант 6xR15 4x98 ET33 DIA58.5 </h2>
+                            <div class="icon-zvezda">
+                                5.0 <img src="img/icon/icon_zbezga.png" alt="" width="30px" height="30px">
+                            </div>
+                            <p class="text-car" >Цена: 7 800</p>
+                            <div class="button">
+                                <a class="cnop1" href="">Купить</a>
+                                <a class="cnop2" href="">Добавить в корзину</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="cartochka2">
+                        <img class="photo1" src="img/cov_dicki/cov_2.png" alt="" width="250px">
+                        <div class="text-cartochka2">
+                            <h2>Колесный диск КиК Хант 6xR15 4x98 ET33 DIA58.5 </h2>
+                            <div class="icon-zvezda">
+                                5.0 <img src="img/icon/icon_zbezga.png" alt="" width="30px" height="30px">
+                            </div>
+                            <p class="text-car" >Цена: 7 800</p>
+                            <div class="button">
+                                <a class="cnop1" href="">Купить</a>
+                                <a class="cnop2" href="">Добавить в корзину</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="cartochka2">
+                        <img class="photo1" src="img/cov_dicki/cov_2.png" alt="" width="250px">
+                        <div class="text-cartochka2">
+                            <h2>Колесный диск КиК Хант 6xR15 4x98 ET33 DIA58.5 </h2>
+                            <div class="icon-zvezda">
+                                5.0 <img src="img/icon/icon_zbezga.png" alt="" width="30px" height="30px">
+                            </div>
+                            <p class="text-car" >Цена: 7 800</p>
+                            <div class="button">
+                                <a class="cnop1" href="">Купить</a>
+                                <a class="cnop2" href="">Добавить в корзину</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="stock">
+            <h1>Акции</h1>
+            <div class="slider">
+            <div class="slides">
+                 <div class="slide active">
+                    <img src="img/slaider/image 11.png" alt="Slide 1">
+                    <div class="caption">
+                        <h3 class="zag1">-20% на литые диски к летнему сезону!</h3>
+                        <p class="text-zag">Обнови стиль и безопасность своего авто! Только до 30 июня — скидки на популярные модели литых дисков R15–R20.</p>
+                    </div>
+                </div>
+                <div class="slide">
+                    <img src="img/slaider/image 12.jpg" alt="Slide 2">
+                    <div class="caption">
+                        <h2 class="zag2">Премиум-диски RAYS и OZ — со скидкой до 30%</h2>
+                        <p class="text-zag2" >Эксклюзивные предложения на элитные бренды. Бери стиль и качество в одном комплекте — по лучшей цене.</p>
+                    </div>
+                </div>
+            </div>
+            <button class="prev"><img src="img/icon/free-icon-font-angle-left.png" alt="" width="30px" ></button>
+            <button class="next"><img src="img/icon/free-icon-font-angle-right (1).png" alt="" width="30px" ></button>
+            </div>
+        </div>  
+
+        <div class="primus">
+            <div class="conteniar">
+                <h1 class="zag3" >Наши премущества</h1>
+                <div class="primus-conteniner">
+                    <div class="primus1">
+                        <div class="icon-title">
+                            <div class="icon-primus">
+                                <img src="img/icon/ass.png" alt="" width="35px" >
+                            </div>
+                            <h1>Широкий ассортимент</h1>
+                        </div>
+                        <p>Более 10 000 моделей автодисков от ведущих мировых брендов – для любого автомобиля и на любой вкус.</p>
+                    </div>
+
+                    <div class="primus1">
+                        <div class="icon-title">
+                            <div class="icon-primus">
+                                <img src="img/icon/garant.png" alt="" width="35px" >
+                            </div>
+                            <h1>Гарантия качества</h1>
+                        </div>
+                        <p>Продаем только оригинальную сертифицированную продукцию. Все диски проходят проверку на соответствие стандартам безопасности.</p>
+                    </div>
+
+                    <div class="primus1">
+                        <div class="icon-title">
+                            <div class="icon-primus">
+                                <img class="icon-doctavka" src="img/icon/doctavka.png" alt="" width="40px" >
+                            </div>
+                            <h1>Быстрая доставка</h1>
+                        </div>
+                        <p>Отправка по всей России и странам СНГ. В наличии – популярные модели, доставка от 1 дня.</p>
+                    </div>
+
+                    <div class="primus1">
+                        <div class="icon-title">
+                            <div class="icon-primus">
+                                <img src="img/icon/cart.png" alt="" width="35px" >
+                            </div>
+                            <h1>Удобные способы оплаты</h1>
+                        </div>
+                        <p>Принимаем карты, рассрочку, оплату при получении. Возможность покупки в кредит.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="komn" id="komn" >
+            <div class="conteniar">
+                <div class="o_komn">
+                    <h1>О компании</h1>
+                    <div class="content-komn">
+                        <p>🏆 Лидер на рынке автомобильных дисков!
+                        Добро пожаловать в WheelHub! Мы – команда профессионалов, которые более 10 лет помогают автолюбителям находить идеальные диски для своих автомобилей.</p>
+                        <div class="image-komn">
+                            <img src="img/image 3.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="contact" id="contact">
+            <div class="conteniar">
+                <div class="text-contact">
+                    <h1>Контакты</h1>
+                    <div class="contacts">
+                        <div class="contact-text">
+                            <h2>Наши контакты</h2>
+                            <p>Email: support@yandex.ru</p>
+                            <p>Адрес: Береговая краснокаменскеая</p>
+                            <div class="grafic">
+                                <ul>
+                                    <p>График работы</p>
+                                    <li>пн-пт: 9:00 - 20:00</li>
+                                    <li>cб-вс: 10:00 - 19:00</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="contact-map">
+                            <iframe
+                            src="https://yandex.ru/map-widget/v1/?um=constructor%3Aпример&source=constructor"
+                            width="700"
+                            height="700px"
+                            frameborder="0">
+                            </iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="otzif">
+            <div class="conteniar">
+                <div class="text-otzif">
+                    <h1>Отзывы</h1>
+                    <div class="content-otzif">
+                        <div class="content1">
+                            <div class="image-content">
+                                <img src="img/lico1.jpg" alt="" width="250px" height="250px">
+                            </div>
+                            <h2>Александр</h2>
+                            <p>Заказал комплект литых дисков на свою Mazda 6 — все пришло в оригинальной упаковке, без повреждений. Сотрудник помог подобрать модель, идеально подошла. Отправили в тот же день, доставили за 4 дня. Очень доволен!</p>
+                        </div>
+
+                         <div class="content1">
+                            <div class="image-content">
+                                <img src="img/lico2.jpg" alt="" width="250px" height="250px">
+                            </div>
+                            <h2>Дмитрий</h2>
+                            <p>Покупал кованые диски на BMW — качество отличное, цена ниже, чем в офлайн-магазинах. Доставили за 7 дней вместо 4, но всё компенсировала бесплатная доставка и скидка за комплект. Рекомендую!</p>
+                        </div>
+
+                         <div class="content1">
+                            <div class="image-content">
+                                <img src="img/lico3.jpg" alt="" width="250px" height="250px">
+                            </div>
+                            <h2>Ксения</h2>
+                            <p>Не разбираюсь в характеристиках, просто ввела марку и модель авто — сайт сам подобрал подходящие диски. Установили на шиномонтаже без проблем, смотрятся 🔥. Отдельно плюс за вежливую поддержку!</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <footer class="footer">
+        <div class="conteniar">
+            <div class="content-footer">
+                <div class="content1">
+                    <img src="img/logo.png" alt="" width="200px" height="130px">
+                </div>
+
+                <div class="content2">
+                    <div class="icon">
+                        <img src="img/icon/icon-cart.png" alt="" width="35px">
+                    </div>
+                    <div class="text-footer">
+                        <p>195027, Россия, Санкт-Петербург, Большеохтинский пр. 35</p>
+                    </div>
+                </div>
+
+                <div class="content2">
+                    <div class="icon">
+                        <img src="img/icon/phone.png" alt="" width="35px">
+                    </div>
+                    <div class="text-footer">
+                        <p>+7 (800) 775-12-16 </p>
+                        <p>+7 (981) 953-26-86</p>
+                    </div>
+                </div>
+
+                <div class="content2">
+                    <div class="icon">
+                        <img src="img/icon/email.png" alt="" width="35px">
+                    </div>
+                    <div class="text-footer">
+                        <p>sale@raenwheels.ru</p>
+                        <p>info@raenwheels.ru</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="js/main.js" ></script>
+</body>
+</html>
